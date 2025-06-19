b456ebdcc74f210edca369fd2a1c8fad7e862aae
Abhi Jha <abhijha283204@gmail.com>
2025-06-19 21:51:18 +0530

    the work is under processing

--- majorityelement.cpp
+++ majorityelement.cpp
@@ -28,14 +28,42 @@
 //   return 0 ;
 
 // }
-#include <iostream>
-#include <vector>
-using namespace std;
-vector<int> pairsumbrute(vector<int> &nums, int target)
-{
-}
-int main()
-{
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// vector<int> pairsumbrute(vector<int> nums, int target)
+// {
+//   vector<int>ans;
+//   int n = nums.size();
+//   int i = 0, j = n-1;
+//   while (i < j)
+//   {
+//     int pairsumbrute = nums[i] + nums[j];
+//     if (pairsumbrute > target)
+//     {
+//      j--;
+//     }
+//     else if (pairsumbrute > target)
+//     {
+//       i++;
+//     }
+//     else
+//     {
+//       ans.push_back(i);
+//       ans.push_back(j);
+//       return ans;
+//     }
+//   }
+//   return ans;
+// }
+// int main()
+// {
+//    vector<int>nums= { 2,9,14,46,75};
+//   int target = 121;
 
-  return 0;
-}
+//    vector<int>ans = pairsumbrute(nums,target);
+//    cout << ans[0] <<  " " << ans[1] << endl ;
+//    return 0 ;
+
+  
+// }
