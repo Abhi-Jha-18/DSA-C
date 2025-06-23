976b77e3854af94db5053cdddc8fcf4a86838767
Abhi Jha <abhijha283204@gmail.com>
2025-06-23 10:52:08 +0530

    the file flow into the valley of dsa

--- practice.cpp
+++ practice.cpp
@@ -1,35 +1,49 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int recbinaryfinder(vector<int>arr,int target,int st, int end){
+int binarysoretedarray(vector<int> &arr, int target)
+{
     int n = arr.size();
-    if (st <= end)
-    {
-        int mid = st + (end - st )/2 ;
-        if (target > arr[mid])
+    int st = 0, end = n - 1;
+    
+    while (st <= end)
+    
+    {   int mid = st + (end - st) / 2;
+        if (arr[mid] == target)
         {
-            return recbinaryfinder(arr,target, mid+1,end);
-        }
-        else if (target < arr[mid])
-        {
-            return recbinaryfinder(arr,target,st,mid -1);
-        }
-        else {
             return mid;
         }
-        
-    }
+        if (arr[st] <= arr[mid])
+        {
 
+            if (arr[st] <= target && target <= arr[mid])
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                st = mid + 1;
+            }
+        }
+        else
+        {
+            if (arr[mid] <= target && target <= arr[end])
+            {
+                st = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+    }
     return -1;
 }
+
 int main()
 {
-  vector<int> arr1 = {1, 2, 4, 5, 6, 7, 8, 7}; // even
-    int target1 = 8;
-   cout << recbinaryfinder(arr1,target1,0,7) << endl;
-    vector<int> arr2 = {1, 2, 4, 5, 6, 7, 63}; // odd
-    int target2 = 7;
-    cout << recbinaryfinder(arr2, target2,0,6) << endl;
-    
+    vector<int> arr1 = {4, 5, 6, 7, 0, 1, 2}; // even
+    int target1 = 5;
+    cout << binarysoretedarray(arr1, target1) << endl;
     return 0;
 }
